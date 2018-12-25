86e30c437c11e2752989a8434f271ee57e074b46
Tadavarthy yaswanth <y11acs505@gmail.com>
2018-12-26 00:18:36 +0530

    added mongoose ORM tool and connecting the database to node js server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const route = express.Router();
 const PORT = 5000;
+const URL = "mongodb://localhost/exercise";
+const mongoose = require("mongoose");
 const personalityQuizData = require("./personality_test");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", route);
 
-app.get("/api/questions", (req, res) => {
+route.get("/questions", (req, res) => {
   res.send(personalityQuizData);
 });
+
+mongoose.connect(
+  URL,
+  (err, db) => {
+    if (err) {
+      console.log("database connection failed", err);
+    } else {
+      console.log(db.collection("type"));
+      db.close();
+    }
+  }
+);
 
 app.listen(PORT, () => {
   console.log(PORT);
